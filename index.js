0b3d7798a17e4ffec0077e5e2440fc35663c783b
JoshuaFowler <jfowler8912@gmail.com>
2023-03-05 20:40:01 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// body.removeChild (main)
+// const html = body.getElementsByTagName("main")[0];
+// const secondChild = ul.querySelector("main");
+// ul.removeChild(secondChild);
+const main = document.querySelector("main#main")
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Josh is the champion";
+console.log(newHeader.textContent)
